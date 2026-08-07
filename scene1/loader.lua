6fecf08e0f30c8f715c24d803f3c54ed279db997
Federico Scodelaro <federicoscodelaro@gmail.com>
2026-08-07 14:35:46 -0300

    feat: Use nativefs to access any file

--- scene1/loader.lua
+++ scene1/loader.lua
@@ -1,3 +1,5 @@
+local nativefs = require("nativefs")
+
 return function(file)
 	if file == nil then
 		return nil, "You have to provide a valid scene file to run"
@@ -6,19 +8,27 @@ return function(file)
 	return {
 		filePath = file,
 		fileLastModified = 0,
+		fileLastCheck = 0,
+		fileCheckIntervalSeconds = 1,
 		scene = nil,
 		canvas = nil,
 
 		update = function(self)
 			-- hotreload file based on https://github.com/kjarvi/monocle/blob/master/monocle.lua
-			local fileInfo = love.filesystem.getInfo(self.filePath)
+			local now = os.time()
+			if now - self.fileLastCheck < self.fileCheckIntervalSeconds then
+				return false
+			end
+
+			self.fileLastCheck = now
+			local fileInfo = nativefs.getInfo(self.filePath)
 			if fileInfo ~= nil and self.fileLastModified ~= fileInfo.modtime then
 				if self.canvas ~= nil then
 					self.canvas:release()
 				end
 
 				self.fileLastModified = fileInfo.modtime
-				self.scene = love.filesystem.load(self.filePath)()
+				self.scene = loadfile(self.filePath)()
 				self.canvas = love.graphics.newCanvas(self.scene.width, self.scene.height)
 				return true
 			end
