4c6cba5ee782614f418fd5620ae5546c2ca68466
Federico Scodelaro <federicoscodelaro@gmail.com>
2026-08-15 18:56:04 -0300

    feat: Add circle object

---
+++ engine/objects/circle.lua
@@ -0,0 +1,40 @@
+local circle = {
+	x = function(time) return 0 end,
+	y = function(time) return 0 end,
+	radius = function(time) return 100 end,
+
+	background = {0,0,0,1},
+
+	strokeColor = {1,0,0,1},
+	strokeWidth = function(time) return 1 end,
+}
+circle.__index = circle
+
+function circle:draw(time)
+	local x = self.x(time)
+	local y = self.y(time)
+	local radius = self.radius(time)
+
+	love.graphics.setColor(self.background)
+	love.graphics.circle(
+		"fill",
+		x,
+		y,
+		radius
+	)
+
+	love.graphics.setColor(self.strokeColor)
+	love.graphics.setLineWidth(self.strokeWidth(time))
+	love.graphics.circle(
+		"line",
+		x,
+		y,
+		radius	
+	)
+end
+
+return function(opts)
+	local o = opts or {}
+	setmetatable(o, circle)
+	return o
+end
--- engine/scene.lua
+++ engine/scene.lua
@@ -1,4 +1,5 @@
 local rectangle = require("objects.rectangle")
+local circle = require("objects.circle")
 local timeline = require("timeline")
 
 local scene = {}
@@ -38,6 +39,53 @@ end
 function scene:add(o)
 	table.insert(self.objs, o)
 end
+
+function scene:circle(o)
+	if o.x == nil then
+		o.x = 0
+	end
+	if type(o.x) == "number" then
+		o.x = timeline(o.x)
+		o.x.context = self.context
+	end
+
+	if o.y == nil then
+		o.y = 0
+	end
+	if type(o.y) == "number" then
+		o.y = timeline(o.y)
+		o.y.context = self.context
+	end
+
+	if o.radius == nil then
+		o.radius = 100
+	end
+	if type(o.radius) == "number" then
+		o.radius = timeline(o.radius)
+		o.radius.context = self.context
+	end
+
+	if o.strokeWidth == nil then
+		o.strokeWidth = 0
+	end
+	if type(o.strokeWidth) == "number" then
+		o.strokeWidth = timeline(o.strokeWidth)
+		o.strokeWidth.context = self.context
+	end
+
+	if o.background == nil then
+		o.background = {1,1,1,1}
+	end
+	if o.strokeColor == nil then
+		o.strokeColor = {0,0,0,1}
+	end
+
+	local c = circle(o)
+	self:add(c)
+	return c
+end
+
+
 
 function scene:rectangle(o)
 	if o.x == nil then
