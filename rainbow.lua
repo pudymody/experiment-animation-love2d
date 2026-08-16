2c895f1adb265874a99db10ebde24149ec2bff67
Federico Scodelaro <federicoscodelaro@gmail.com>
2026-08-16 00:19:18 -0300

    docs: Update demo

--- rainbow.lua
+++ rainbow.lua
@@ -1,33 +1,30 @@
 local newScene = require("scene")
 local easing = require("easing")
+local colors = require("colors")
 
-local scene = newScene {
-	width = 1920,
-	height = 1080,
-	fps = 60,
-	background = {1,1,1,1},
+local scene = newScene {}
+
+local rainbowColors = {
+	colors.new {255,0,0,255},
+	colors.new {255/255,127/255,0,255/255},
+	colors.new {255,255,0,255},
+	colors.new {0,255,0,255},
+	colors.new {0,0,255,255},
+	colors.new {139,0,255,255},
 }
 
-local colors = {
-	{255,0,0,255},
-	{255/255,127/255,0,255/255},
-	{255,255,0,255},
-	{0,255,0,255},
-	{0,0,255,255},
-	{139,0,255,255},
-}
-
-local squareWidth = scene.width / #colors
+local squareWidth = scene.width / #rainbowColors
 local duration = 875
 local interval = 125
 
-for i,c in ipairs(colors) do
+for i,c in ipairs(rainbowColors) do
 	local r = scene:rectangle {
 		x = squareWidth * (i-1),
 		y = scene.height,
 		width = squareWidth,
 		height = 0,
-		background = c
+		background = c,
+		strokeWidth = 0,
 	}
 
 	r.y:to {
